e01f8af921a0b4c4f9451940e5bf6327aeca6753
AbelSantiagoG <abelsantiago1000@gmail.com>
2023-04-28 11:40:57 -0500

    Se agregó binary_tree.py

---
+++ Corte III/tree-structure/py/main.py
@@ -0,0 +1,26 @@
+from binary_tree import BinaryTree
+
+tree= BinaryTree(35)
+
+tree.insert(tree, BinaryTree(5))
+tree.insert(tree, BinaryTree(20))
+tree.insert(tree, BinaryTree(19))
+tree.insert(tree, BinaryTree(42))
+tree.insert(tree, BinaryTree(36))
+tree.insert(tree, BinaryTree(31))
+tree.insert(tree, BinaryTree(35))
+tree.insert(tree, BinaryTree(30))
+tree.insert(tree, BinaryTree(33))
+tree.insert(tree, BinaryTree(101))
+tree.insert(tree, BinaryTree(46))
+tree.insert(tree, BinaryTree(29))
+tree.insert(tree, BinaryTree(27))
+tree.insert(tree, BinaryTree(98))
+tree.insert(tree, BinaryTree(37))
+tree.insert(tree, BinaryTree(16))
+
+
+
+
+
+tree.print_tree(tree)
